0f6c48bbd34a79a97088f4994c21939521a54e60
vnnico <nicson7585@gmail.com>
2024-07-10 22:35:40 +0700

    Creating an User Object after Google Login

--- backend/config/databaseConfig.js
+++ backend/config/databaseConfig.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const database = new Sequelize("sxcdatabase", "root", "root", {
+const database = new Sequelize("sxcdatabase", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
--- backend/config/passport.js
+++ backend/config/passport.js
@@ -9,33 +9,48 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "http://localhost:3001/auth/google/callback",
     },
-    (accessToken, refreshToken, profile, done) => {
-      // Logic to find or create user in database
-      //   User.findOrCreate({ id: profile.id }, (err, user) => {
-      //     return done(err, user);
-      //   });
-      console.log(profile);
-      return done(null, profile);
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const currentUser = await User.findOne({
+          where: { oauthId: profile.id },
+        });
+        console.log(currentUser);
+        if (currentUser) {
+          return done(null, currentUser);
+        } else {
+          console.log("masuk sini");
+          const newUser = await User.create({
+            oauthId: profile.id,
+            username: profile.name.givenName,
+            email: profile.emails[0].value,
+          });
+
+          await newUser.save();
+          return done(null, newUser);
+        }
+      } catch (error) {
+        return done(error, null);
+      }
     }
   )
 );
 
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-
-passport.deserializeUser((obj, done) => {
-  done(null, obj);
-});
-
 // passport.serializeUser((user, done) => {
-//   done(null, user.id);
+//   done(null, user);
 // });
 
-// passport.deserializeUser((id, done) => {
-//     User.findById(id, (err, user) => {
-//       done(err, user);
-//     });
+// passport.deserializeUser((obj, done) => {
+//   done(null, obj);
 // });
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findByPk(id).then((user) => {
+    done(null, user);
+  });
+});
 
 module.exports = passport;
