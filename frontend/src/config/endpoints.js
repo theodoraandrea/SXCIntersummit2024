2c85f6899901299a96fc5baf67e73eac5b2b4f6a
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2024-07-19 04:16:48 +0700

    Added endpoint config file for modularity

---
+++ frontend/src/config/endpoints.js
@@ -0,0 +1,9 @@
+const API_URL = "http://localhost:3001";
+
+const GOOGLE_LOGIN = "/auth/google";
+const PROFILE_DATA = "/profile";
+
+const API_GOOGLE_LOGIN = `${API_URL}${GOOGLE_LOGIN}`;
+const API_PROFILE_DATA = `${API_URL}${PROFILE_DATA}`;
+
+export { API_GOOGLE_LOGIN };
