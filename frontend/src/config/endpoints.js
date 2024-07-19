9bced155b064a96232d57623787c667637c35b41
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2024-07-19 22:46:33 +0700

    Added more API endpoints to FE

--- frontend/src/config/endpoints.js
+++ frontend/src/config/endpoints.js
@@ -1,9 +1,12 @@
 const API_URL = "http://localhost:3001";
 
 const GOOGLE_LOGIN = "/auth/google";
+const LOGOUT = "/auth/logout";
+
 const PROFILE_DATA = "/profile";
 
 const API_GOOGLE_LOGIN = `${API_URL}${GOOGLE_LOGIN}`;
+const API_LOGOUT = `${API_URL}${LOGOUT}`;
 const API_PROFILE_DATA = `${API_URL}${PROFILE_DATA}`;
 
-export { API_GOOGLE_LOGIN };
+export { API_GOOGLE_LOGIN, API_LOGOUT, API_PROFILE_DATA };
