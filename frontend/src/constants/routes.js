28ad6414fe1af3a5a700e88a1f04c4f5ea36168c
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2024-07-21 04:48:57 +0700

    added routes.js constant for centralized configuration

---
+++ frontend/src/constants/routes.js
@@ -0,0 +1,11 @@
+export const HOME = "/";
+export const HOME2 = "/home";
+export const HOME3 = "/register1/register2/home";
+
+export const ABOUT = "/about";
+export const REGISTER = "/register1";
+export const FILL_DETAILS = "/register1/register2";
+export const USER_DASHBOARD = "/user-dashboard";
+export const USER_PROFILE = "/user-dashboard/profile";
+export const USER_SETTINGS = "/user-dashboard/settings";
+export const USER_EVENTS = "/user-dashboard/events";
