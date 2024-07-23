47bfbe265fb47bd9d16ed02062e712863e30e41c
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2024-07-23 21:01:21 +0700

    feat: (backend) callback url now uses env variable

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3001/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
