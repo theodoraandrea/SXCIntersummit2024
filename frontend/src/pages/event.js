3472b38f05a1f14b640668d067e6a4cc93eb5818
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2024-08-01 05:02:59 +0700

    feat: connected events page with backend API

--- frontend/src/pages/event.js
+++ frontend/src/pages/event.js
@@ -3,12 +3,14 @@ import { useLocation } from "react-router-dom";
 import EventCard from "./../components/elements/event-card";
 import Navbar from "./../components/navbar";
 import Footer from "./../components/footer";
-import { DummyEventsData as events } from "../constants/dummy/eventsdata";
+// import { DummyEventsData as events } from "../constants/dummy/eventsdata";
 import { fetchAllEvents } from "../service/services";
 
 const Events = () => {
   const location = useLocation();
   const [filter, setFilter] = useState("All");
+  const [eventsData, setEventsData] = useState(null);
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
     const query = new URLSearchParams(location.search);
@@ -18,20 +20,34 @@ const Events = () => {
     }
   }, [location.search]);
 
-  const filteredEvents =
-    filter === "All"
-      ? events
-      : events.filter((event) => event.category === filter);
+  const fetchAllEventsData = async () => {
+    try {
+      const response = await fetchAllEvents();
+      setEventsData(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllEventsData();
+  }, []);
+
+  useEffect(() => {
+    let filteredEvents =
+      filter === "All"
+        ? eventsData
+        : eventsData.filter((event) => event.category === filter);
+    setFilteredData(filteredEvents);
+  }, [eventsData, filter]);
 
   return (
     <>
-      <Navbar 
-        currentPath={location.pathname}
-      />
+      <Navbar currentPath={location.pathname} />
       <div className="p-8 bg-primary-1 text-white min-h-screen">
         <div className="flex mx-auto">
           <div className="flex space-x-4 mb-4 mx-auto">
-            {["All", "Workshop", "Company Visit", "Competitions"].map(
+            {["All", "Workshop", "Company Visit", "Competition"].map(
               (category) => (
                 <button
                   key={category}
