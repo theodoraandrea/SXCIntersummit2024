d35efe083d595ce6dc2496ece0998a352c3d334f
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2024-07-31 17:45:31 +0700

    feat: added logic to connect events page with backend API

--- frontend/src/constants/dummy/eventsdata.js
+++ frontend/src/constants/dummy/eventsdata.js
@@ -4,17 +4,35 @@ export const DummyEventsData = [
     eventName: "Future CEO",
     date: "2024-09-19",
     location: "Jakarta Timur",
+    shortDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus in dolor vel semper. Donec augue neque, fermentum sed augue a, cursus fermentum nunc.",
+    category: "Competitions",
   },
   {
     id: 2,
     eventName: "Chambers",
     date: "2024-7-09",
     location: "Jakarta Barat",
+    shortDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus in dolor vel semper. Donec augue neque, fermentum sed augue a, cursus fermentum nunc.",
+    category: "Competitions",
   },
   {
     id: 3,
     eventName: "Company Visit",
     date: "2024-12-28",
     location: "Merauke",
+    shortDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus in dolor vel semper. Donec augue neque, fermentum sed augue a, cursus fermentum nunc.",
+    category: "Company Visit",
+  },
+  {
+    id: 4,
+    eventName: "New Workshop Event",
+    date: "2024-12-28",
+    location: "Merauke",
+    shortDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus in dolor vel semper. Donec augue neque, fermentum sed augue a, cursus fermentum nunc.",
+    category: "Workshop",
   },
 ];
--- frontend/src/pages/event.js
+++ frontend/src/pages/event.js
@@ -1,51 +1,72 @@
 // src/Events.js
-import React, { useState } from 'react';
-import EventCard from './../components/elements/event-card';
+import React, { useEffect, useState } from "react";
+import EventCard from "./../components/elements/event-card";
 import Navbar from "./../components/navbar";
 import Footer from "./../components/footer";
-
-const events = [
-  { title: 'Event 01', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus in dolor vel semper. Donec augue neque, fermentum sed augue a, cursus fermentum nunc.', category: 'Workshop' },
-  { title: 'Event 02', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus in dolor vel semper. Donec augue neque, fermentum sed augue a, cursus fermentum nunc.', category: 'Company Visit' },
-  { title: 'Event 03', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus in dolor vel semper. Donec augue neque, fermentum sed augue a, cursus fermentum nunc.', category: 'Competitions' },
-];
+import { DummyEventsData as events } from "../constants/dummy/eventsdata";
+import { fetchAllEvents } from "../service/services";
 
 const Events = () => {
-  const [filter, setFilter] = useState('All');
+  const [filter, setFilter] = useState("All");
+  const [eventsData, setEventsData] = useState(null);
+  const [filteredData, setFilteredData] = useState([]);
 
-  const filteredEvents = filter === 'All'
-    ? events
-    : events.filter(event => event.category === filter);
+  const fetchAllEventsData = async () => {
+    try {
+      // const response = await fetchAllEvents();
+      setEventsData(events);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllEventsData();
+  }, []);
+
+  useEffect(() => {
+    let filteredEvents =
+      filter === "All"
+        ? eventsData
+        : eventsData.filter((event) => event.category === filter);
+    setFilteredData(filteredEvents);
+  }, [eventsData, filter]);
 
   return (
     <>
       <Navbar />
       <div className="p-8 bg-primary-1 text-white min-h-screen">
-        <div className='flex mx-auto'>
+        <div className="flex mx-auto">
           <div className="flex space-x-4 mb-4 mx-auto">
-            {['All', 'Workshop', 'Company Visit', 'Competitions'].map(category => (
+            {["All", "Workshop", "Company Visit", "Competitions"].map(
+              (category) => (
                 <button
                   key={category}
                   onClick={() => setFilter(category)}
-                  className={`px-4 py-2 rounded ${filter === category ? 'bg-yellow-500' : 'bg-teal-700'}`}
+                  className={`px-4 py-2 rounded ${
+                    filter === category ? "bg-yellow-500" : "bg-teal-700"
+                  }`}
                 >
                   {category}
                 </button>
-            ))}
+              )
+            )}
           </div>
         </div>
         <div className="space-y-4 my-5">
-          {filteredEvents.map((event, index) => (
-            <EventCard
-              key={index}
-              title={event.title}
-              description={event.description}
-              category={event.category}
-            />
-          ))}
+          {filteredData && filteredData.length > 0
+            ? filteredData.map((event, index) => (
+                <EventCard
+                  key={index}
+                  title={event.eventName}
+                  description={event.shortDesc}
+                  category={event.category}
+                />
+              ))
+            : null}
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 };
