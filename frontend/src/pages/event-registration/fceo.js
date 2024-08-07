a6a4c95b8bfc06aa76dc52356b4c5a7e5d330b78
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2024-08-07 22:45:49 +0700

    fix: missing useEffect

--- frontend/src/pages/event-registration/fceo.js
+++ frontend/src/pages/event-registration/fceo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/navbar";
 import Spinner from "../../components/elements/spinner";
 import { useNavigate } from "react-router-dom";
