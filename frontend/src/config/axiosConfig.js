7dd6c3621fbb515181c72e1c671d3b67aadbf7e5
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2024-08-11 05:48:32 +0700

    feat: installed ngrok

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -4,19 +4,23 @@ import { API_URL } from "./endpoints";
 const axiosInstance = axios.create({
   baseURL: API_URL,
   headers: {
+    "ngrok-skip-browser-warning": "true",
     "Content-Type": "application/json",
   },
 });
 
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    console.log("set bearer token ", token);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  console.log("set bearer token ", token);
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default axiosInstance;
