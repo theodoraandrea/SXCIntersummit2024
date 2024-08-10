468709661114bbac823e89e1208ee23546e23a8c
vnnico <nicson7585@gmail.com>
2024-08-10 13:47:02 +0700

    docs: multer middleware

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,6 +1,23 @@
 const multer = require("multer");
 const path = require("path");
 
+/**
+ * In this Multer Middleware:
+ * 1. Checking File Type
+ * 2. Checking File Size
+ *
+ * Brief Guide:
+ * 1. Make sure to put upload() from multer function before body() from express-validator.
+ *
+ * Reason:
+ * When handling multipart/form-data requests, Multer is responsible for processing and extracting files and form fields from the request. If the express-validator's body() is executed before Multer, the form data will not yet be available for validation because it has not been processed.
+ * By placing upload() before body(), we ensure that all form fields are extracted and accessible for validation by express-validator.
+ *
+ *
+ * 2. Import and use errorHandling Middleware in the the route to catch Multer Errors (e.g file size and file type errors).
+ * 3. In the controllers, import checkRequiredFields from utils to ensure all required images/files are uploaded without exception.
+ */
+
 function checkFileType(file, cb) {
   const filetypes = /jpeg|jpg|png|pdf/;
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
