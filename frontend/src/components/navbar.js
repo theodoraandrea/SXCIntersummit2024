b2cd655aa368ea735088011fbc716340c54fd7e0
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2024-08-13 23:11:18 +0700

    reformat: reformat codes in navbar.js

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -26,7 +26,106 @@ export default function NavbarUser({ currentPath }) {
 
   return (
     <nav className="bg-primary-1 w-full h-16 flex items-center px-5 md:px-10 sticky top-0 z-50">
-      <img src={logo} alt="logo" className="w-14 md:w-16" />
+      <Link to={HOME}>
+        <img src={logo} alt="logo" className="w-14 md:w-16" />
+      </Link>
+
+      {/* Navigation Links for Desktop View */}
+      <ul className="hidden md:flex flex-grow items-center space-x-10 ml-10">
+        <li>
+          <Link
+            to={HOME}
+            className={`text-white hover:font-semibold ${
+              currentPath === HOME
+                ? "font-semibold text-yellow-500"
+                : "hover:text-yellow-500"
+            }`}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            to={ABOUT_PAGE}
+            className={`text-white hover:font-semibold ${
+              currentPath === ABOUT_PAGE
+                ? "font-semibold text-yellow-500"
+                : "hover:text-yellow-500"
+            }`}
+          >
+            About
+          </Link>
+        </li>
+        <li className="relative">
+          <button
+            onClick={toggleDropdown}
+            className={`flex items-center text-white hover:font-semibold ${
+              currentPath === EVENTS_PAGE
+                ? "font-semibold text-yellow-500"
+                : "hover:text-yellow-500"
+            }`}
+          >
+            Events
+            <svg
+              className={`w-4 h-4 ml-2 transition-transform ${
+                dropdownOpen ? "rotate-180" : "rotate-0"
+              }`}
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 9l-7 7-7-7"
+              ></path>
+            </svg>
+          </button>
+          {dropdownOpen && (
+            <div className="absolute right-0 mt-2 w-48 bg-white text-black rounded shadow-lg z-10">
+              <Link
+                to={`${EVENTS_PAGE}?category=All`}
+                className="block px-4 py-2 hover:bg-gray-200"
+                onClick={() => setDropdownOpen(false)}
+              >
+                All
+              </Link>
+              <Link
+                to={`${EVENTS_PAGE}?category=Workshop`}
+                className="block px-4 py-2 hover:bg-gray-200"
+                onClick={() => setDropdownOpen(false)}
+              >
+                Workshop
+              </Link>
+              <Link
+                to={`${EVENTS_PAGE}?category=Company Visit`}
+                className="block px-4 py-2 hover:bg-gray-200"
+                onClick={() => setDropdownOpen(false)}
+              >
+                Company Visit
+              </Link>
+              <Link
+                to={`${EVENTS_PAGE}?category=Competition`}
+                className="block px-4 py-2 hover:bg-gray-200"
+                onClick={() => setDropdownOpen(false)}
+              >
+                Competitions
+              </Link>
+            </div>
+          )}
+        </li>
+
+        <li>
+          <Link
+            to="#"
+            className="text-white hover:font-semibold hover:text-yellow-500"
+          >
+            Merchandise
+          </Link>
+        </li>
+      </ul>
 
       {/* Burger Menu for Mobile View */}
       <button className="text-white md:hidden ml-auto" onClick={toggleMenu}>
@@ -160,103 +259,6 @@ export default function NavbarUser({ currentPath }) {
           </ul>
         </div>
       )}
-
-      {/* Navigation Links for Desktop View */}
-      <ul className="hidden md:flex flex-grow items-center space-x-10 ml-10">
-        <li>
-          <Link
-            to={HOME}
-            className={`text-white hover:font-semibold ${
-              currentPath === HOME
-                ? "font-semibold text-yellow-500"
-                : "hover:text-yellow-500"
-            }`}
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            to={ABOUT_PAGE}
-            className={`text-white hover:font-semibold ${
-              currentPath === ABOUT_PAGE
-                ? "font-semibold text-yellow-500"
-                : "hover:text-yellow-500"
-            }`}
-          >
-            About
-          </Link>
-        </li>
-        <li className="relative">
-          <button
-            onClick={toggleDropdown}
-            className={`flex items-center text-white hover:font-semibold ${
-              currentPath === EVENTS_PAGE
-                ? "font-semibold text-yellow-500"
-                : "hover:text-yellow-500"
-            }`}
-          >
-            Events
-            <svg
-              className={`w-4 h-4 ml-2 transition-transform ${
-                dropdownOpen ? "rotate-180" : "rotate-0"
-              }`}
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M19 9l-7 7-7-7"
-              ></path>
-            </svg>
-          </button>
-          {dropdownOpen && (
-            <div className="absolute right-0 mt-2 w-48 bg-white text-black rounded shadow-lg z-10">
-              <Link
-                to={`${EVENTS_PAGE}?category=All`}
-                className="block px-4 py-2 hover:bg-gray-200"
-                onClick={() => setDropdownOpen(false)}
-              >
-                All
-              </Link>
-              <Link
-                to={`${EVENTS_PAGE}?category=Workshop`}
-                className="block px-4 py-2 hover:bg-gray-200"
-                onClick={() => setDropdownOpen(false)}
-              >
-                Workshop
-              </Link>
-              <Link
-                to={`${EVENTS_PAGE}?category=Company Visit`}
-                className="block px-4 py-2 hover:bg-gray-200"
-                onClick={() => setDropdownOpen(false)}
-              >
-                Company Visit
-              </Link>
-              <Link
-                to={`${EVENTS_PAGE}?category=Competition`}
-                className="block px-4 py-2 hover:bg-gray-200"
-                onClick={() => setDropdownOpen(false)}
-              >
-                Competitions
-              </Link>
-            </div>
-          )}
-        </li>
-
-        <li>
-          <Link
-            to="#"
-            className="text-white hover:font-semibold hover:text-yellow-500"
-          >
-            Merchandise
-          </Link>
-        </li>
-      </ul>
 
       {/* User Actions */}
       <div className="hidden md:flex ml-auto flex-col md:flex-row items-center space-x-4 space-y-2 md:space-y-0">
