d0ec5e669af830feae656c5f7079aa723f138d61
theodoraandrea <theodora.lydia@student.sgu.ac.id>
2024-08-17 16:10:04 +0200

    * disable register for users that are not logged in * moved register now button & view booklet button to top of events detail

--- frontend/src/components/elements/timeline-item.js
+++ frontend/src/components/elements/timeline-item.js
@@ -3,30 +3,27 @@ import { Link } from "react-router-dom";
 
 const TimelineItem = ({ title, date, type, link, registered }) => {
   return (
-    <div className="flex items-center">
+    <div className="mx-4 flex items-center">
       <div className="bg-primary-3 w-4 h-4 rounded-full"></div>
-      <div className="ml-4">
+      <div className="ml-4 my-2">
         <h3 className="font-bold">{title}</h3>
         <p>{date}</p>
-        {type === "register" && (
+        {/*type === "register" && (
           <>
           { registered ? (
-              <button className="text-sm md:text-base py-1 px-4 bg-primary-2 md:px-4 md:py-2 rounded-lg text-white mr-2 mt-2"
+              <button className="text-sm md:text-base py-1 px-4 bg-primary-2 md:px-4 md:py-2 my-2 rounded-lg text-white mr-2 mt-2"
               disabled={true}>
               Already registered!
             </button>
-          ) :<Link to={link}
-          >
-            <button className="text-sm md:text-base py-1 px-4 bg-primary-2 md:px-4 md:py-2 rounded-lg text-white mr-2 mt-2">
+          ) :
+          <Link to={link}>
+            <button className="text-sm md:text-base py-1 px-4 bg-primary-2 md:px-4 md:py-2 my-2 rounded-lg text-white mr-2 mt-2">
               Register Now
             </button>
           </Link>
           }
-            <button className="text-sm md:text-base bg-primary-3 py-1 px-4 md:py-2 md:px-4 rounded-lg text-white my-2">
-              View Booklet
-            </button>
           </>
-        )}
+        )*/}
       </div>
     </div>
   );
