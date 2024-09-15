e717638995d13d5acaffb9c85bd41e0bbc58ada0
vnnico <nicson7585@gmail.com>
2024-09-15 14:36:05 +0700

    fixed: Update Chambers Model

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const path = require("path");
 
 const user = require("./models/user");
 const db = require("./config/databaseConfig");
-const IBPC = require("./models/ibpc");
+const Chamber = require("./models/chamber");
 require("./associations/association").eventAssociations();
 require("./associations/association").competitionAssociations();
 
@@ -30,14 +30,14 @@ db.authenticate()
   });
 
 // Database Table synchronizing
-// IBPC.sync({ alter: true })
-//   .then(() => {
-//     console.log("Update db");
-//   })
-//   .catch((err) => {
-//     console.log("ERROR");
-//     console.log(err.message);
-//   });
+Chamber.sync({ alter: true })
+  .then(() => {
+    console.log("Update db");
+  })
+  .catch((err) => {
+    console.log("ERROR");
+    console.log(err.message);
+  });
 
 // CORS configuration
 app.use(corsMiddleware);
