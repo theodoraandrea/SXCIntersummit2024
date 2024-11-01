2da86aaa800c02180a76a97b4dfec21b04698b76
theodoraandrea <theodora.lydia@student.sgu.ac.id>
2024-11-01 13:24:14 +0700

    no message

--- backend/controllers/summitControllers.js
+++ backend/controllers/summitControllers.js
@@ -97,7 +97,7 @@ exports.registerSummit = async (req, res) => {
         registrationId: eventRegistration.id,
         //cityOfResidence: body.cityOfResidence,
         status: body.status,
-        statusDetail: body.statusDetail,
+        statusDetail,
         question: qnaList,
         proofOfStory,
         proofOfFollow,
